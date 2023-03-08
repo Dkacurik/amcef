c5fcacc5a6578f03afd0f89bb68aad6c50f6844a
David Kacurik <david.kacurik@gmail.com>
2023-03-08 15:07:04 +0100

    ADD - list user posts

--- app/backend/app/intro/services/post.py
+++ app/backend/app/intro/services/post.py
@@ -5,7 +5,6 @@ from app.intro.models import Post
 
 def validate_user(user_id: int) -> bool:
     r = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
-    response = None
     if r.status_code == 200:
         return True
 
@@ -18,5 +17,28 @@ def create_post(*, user_id: int, title: str, body: str) -> Post:
         post.save()
 
         return post
+
+    return None
+
+
+def get_post(post_id: int) -> Post:
+    r = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
+    response = None
+    post = None
+    if r.status_code == 200:
+        response = r.json()
+        try:
+            post = Post.objects.get(user_id=int(response['userId']), title=response['title'],
+                                    body=response['body'])
+        except:
+            post = create_post(user_id=int(response['userId']), title=response['title'],
+                               body=response['body'])
+
+    return post
+
+
+def get_user_posts(user_id: int):
+    if validate_user(user_id):
+        return Post.objects.filter(user_id=user_id)
 
     return None
--- app/backend/app/intro/viewsets/post.py
+++ app/backend/app/intro/viewsets/post.py
@@ -1,9 +1,12 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from app.intro.models import Post
 from app.intro.serializers.post import PostModelSerializer
-from app.intro.services.post import validate_user, create_post
+from app.intro.services.post import create_post, get_post, get_user_posts
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -16,5 +19,30 @@ class PostViewSet(viewsets.ModelViewSet):
         post = create_post(**serializer.validated_data)
         if post is not None:
             return Response(PostModelSerializer(post).data, status=status.HTTP_201_CREATED)
+
+        return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = get_post(kwargs['pk'])
+        if instance is not None:
+            return Response(PostModelSerializer(instance).data)
+        return Response({'message': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    @swagger_auto_schema(
+        operation_id='user_posts',
+        manual_parameters=[
+            openapi.Parameter('user_id', openapi.IN_QUERY, type=openapi.TYPE_NUMBER, description='user_id'),
+        ],
+        responses={200: PostModelSerializer(many=True)}
+    )
+    @action(detail=False, methods=['get'], url_path='user-posts')
+    def user_posts(self, request, *args, **kwargs):
+        user_id = int(request.query_params.get('user_id', None))
+
+        user_posts = get_user_posts(user_id)
+
+        if user_posts is not None:
+            serializer = PostModelSerializer(user_posts, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
